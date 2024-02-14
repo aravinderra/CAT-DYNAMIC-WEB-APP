8899c8563e8f4bc3b77a65fdbc814fa511186865
aravinderra <145091854+aravinderra@users.noreply.github.com>
2024-02-15 00:59:57 +0530

    Add files via upload

---
+++ CAT Dynamic Web Application.js
@@ -0,0 +1,18 @@
+function offSwitch() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switched Off";
+    document.getElementById("offswitchBackground").style.backgroundColor = "#c9c6c5";
+    document.getElementById("onswitchBackground").style.backgroundColor = "#08bd50";
+
+}
+
+function onSwitch() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "Switched On";
+    document.getElementById("offswitchBackground").style.backgroundColor = "#ed2a0c";
+    document.getElementById("onswitchBackground").style.backgroundColor = "#c9c6c5";
+
+
+}
